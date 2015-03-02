647c9f78c1342c4f2ea616b9354eee205a8ad8f8
Nic Aitch <nic@nicinabox.com>
2015-03-02 15:39:50 -0600

    Refactor to extend prototype

--- src/simplecrop.js
+++ src/simplecrop.js
@@ -25,190 +25,192 @@
     this.onLoad();
   };
 
-  SimpleCrop.prototype.onLoad = function() {
-    this.$el.load($.proxy(function() {
-      this.naturalDimensions = {
+  $.extend(SimpleCrop.prototype, {
+    onLoad: function() {
+      this.$el.load($.proxy(function() {
+        this.naturalDimensions = {
+          width: this.$el.width(),
+          height: this.$el.height()
+        };
+
+        this.orientation = this.getOrientation();
+        this.portrait    = this.orientation === 'portrait';
+        this.landscape   = this.orientation === 'landscape';
+        this.square      = this.orientation === 'square';
+
+        this.setupUI();
+        this.setupDraggable();
+      }, this));
+    },
+
+    destroy: function() {
+      $.removeData(this.$el[0]);
+    },
+
+    getOrientation: function() {
+      var width  = this.naturalDimensions.width,
+          height = this.naturalDimensions.height;
+
+      if (width > height) {
+        return 'landscape';
+      } else if(width < height) {
+        return 'portrait';
+      } else {
+        return 'square';
+      }
+    },
+
+    setupDraggable: function() {
+      var self = this,
+          leftEdge = Math.ceil(self.$constraint.position().left),
+          topEdge  = Math.ceil(self.$constraint.position().top),
+          relativeRight  = this.dimensions.width - self.$constraint.width(),
+          relativeBottom = this.dimensions.height - self.$constraint.height();
+
+      var drag = {
+        start: function(e) {
+          e.preventDefault();
+
+          var rightClick = e.button === 2 || e.which === 3;
+          if (rightClick) { return; }
+
+          var data = {
+            startX: e.clientX,
+            relativeX: (e.clientX - leftEdge) - self.getPosition().left,
+            relativeY: (e.clientY - topEdge) - self.getPosition().top
+          };
+
+          self.$el.trigger('cropstart');
+          $(window)
+            .off('mousemove.simplecrop')
+            .on('mousemove.simplecrop', function(e) {
+              e.data = data;
+              drag.move(e);
+            });
+        },
+
+        move: function(e) {
+          var left = Math.ceil(e.clientX - e.data.relativeX - leftEdge);
+          var top = Math.ceil(e.clientY - e.data.relativeY - topEdge);
+
+          if (self.square) { return; }
+
+          if (left >= -relativeRight && left <= 0) {
+            self.$el.css('left', left + 'px');
+          }
+
+          if (top >= -relativeBottom && top <= 0) {
+            self.$el.css('top', top + 'px');
+          }
+        },
+
+        stop: function() {
+          $(window).off('mousemove.simplecrop');
+          self.$el.trigger('cropend', [self.getDimensions()]);
+        }
+      };
+
+      this.$constraint.on('mousedown.simplecrop', drag.start);
+      $(window).on('mouseup.simplecrop', drag.stop);
+    },
+
+    setupUI: function() {
+      this.resetContainers();
+      this.setMaxDimensions();
+
+      this.dimensions = {
         width: this.$el.width(),
         height: this.$el.height()
       };
 
-      this.orientation = this.getOrientation();
-      this.portrait    = this.orientation === 'portrait';
-      this.landscape   = this.orientation === 'landscape';
-      this.square      = this.orientation === 'square';
-
-      this.setupUI();
-      this.setupDraggable();
-    }, this));
-  };
-
-  SimpleCrop.prototype.destroy = function() {
-    $.removeData(this.$el[0]);
-  };
-
-  SimpleCrop.prototype.getOrientation = function() {
-    var width  = this.naturalDimensions.width,
-        height = this.naturalDimensions.height;
-
-    if (width > height) {
-      return 'landscape';
-    } else if(width < height) {
-      return 'portrait';
-    } else {
-      return 'square';
-    }
-  };
-
-  SimpleCrop.prototype.setupDraggable = function() {
-    var self = this,
-        leftEdge = Math.ceil(self.$constraint.position().left),
-        topEdge  = Math.ceil(self.$constraint.position().top),
-        relativeRight  = this.dimensions.width - self.$constraint.width(),
-        relativeBottom = this.dimensions.height - self.$constraint.height();
-
-    var drag = {
-      start: function(e) {
-        e.preventDefault();
-
-        var rightClick = e.button === 2 || e.which === 3;
-        if (rightClick) { return; }
-
-        var data = {
-          startX: e.clientX,
-          relativeX: (e.clientX - leftEdge) - self.getPosition().left,
-          relativeY: (e.clientY - topEdge) - self.getPosition().top
-        };
-
-        self.$el.trigger('cropstart');
-        $(window)
-          .off('mousemove.simplecrop')
-          .on('mousemove.simplecrop', function(e) {
-            e.data = data;
-            drag.move(e);
-          });
-      },
-
-      move: function(e) {
-        var left = Math.ceil(e.clientX - e.data.relativeX - leftEdge);
-        var top = Math.ceil(e.clientY - e.data.relativeY - topEdge);
-
-        if (self.square) { return; }
-
-        if (left >= -relativeRight && left <= 0) {
-          self.$el.css('left', left + 'px');
-        }
-
-        if (top >= -relativeBottom && top <= 0) {
-          self.$el.css('top', top + 'px');
-        }
-      },
-
-      stop: function() {
-        $(window).off('mousemove.simplecrop');
-        self.$el.trigger('cropend', [self.getDimensions()]);
-      }
-    };
-
-    this.$constraint.on('mousedown.simplecrop', drag.start);
-    $(window).on('mouseup.simplecrop', drag.stop);
-  };
-
-  SimpleCrop.prototype.setupUI = function() {
-    this.resetContainers();
-    this.setMaxDimensions();
-
-    this.dimensions = {
-      width: this.$el.width(),
-      height: this.$el.height()
-    };
-
-    this.$constraint
-      .width(this.options.width)
-      .height(this.options.height)
-      .addClass(this.orientation);
-
-    this.$el.css(this.startingPosition());
-  };
-
-  SimpleCrop.prototype.createContainers = function() {
-    this.$el.wrap('<div class="simplecrop-container"><div class="simplecrop-constraint">');
-    this.getContainers();
-  };
-
-  SimpleCrop.prototype.getContainers = function() {
-    this.$constraint = this.$el.parent('.simplecrop-constraint');
-    this.$container  = this.$el.parents('.simplecrop-container');
-  };
-
-  SimpleCrop.prototype.resetContainers = function() {
-    this.getContainers();
-
-    if (this.$constraint.length) {
       this.$constraint
-        .removeClass('portrait landscape square')
-        .attr('style', '');
-    } else {
-      this.createContainers();
-    }
-  };
+        .width(this.options.width)
+        .height(this.options.height)
+        .addClass(this.orientation);
 
-  SimpleCrop.prototype.startingPosition = function() {
-    var position = {
-          left: 0,
-          top: 0
-        },
-        width  = this.dimensions.width,
-        height = this.dimensions.height;
+      this.$el.css(this.startingPosition());
+    },
 
-    if (!this.square) {
-      if (width > this.options.width) {
-        position.left = (width - this.options.width) * -this.options.offset;
+    createContainers: function() {
+      this.$el.wrap('<div class="simplecrop-container"><div class="simplecrop-constraint">');
+      this.getContainers();
+    },
+
+    getContainers: function() {
+      this.$constraint = this.$el.parent('.simplecrop-constraint');
+      this.$container  = this.$el.parents('.simplecrop-container');
+    },
+
+    resetContainers: function() {
+      this.getContainers();
+
+      if (this.$constraint.length) {
+        this.$constraint
+          .removeClass('portrait landscape square')
+          .attr('style', '');
+      } else {
+        this.createContainers();
+      }
+    },
+
+    startingPosition: function() {
+      var position = {
+            left: 0,
+            top: 0
+          },
+          width  = this.dimensions.width,
+          height = this.dimensions.height;
+
+      if (!this.square) {
+        if (width > this.options.width) {
+          position.left = (width - this.options.width) * -this.options.offset;
+        }
+
+        if (height > this.options.height) {
+          position.top = (height - this.options.height) * -this.options.offset;
+        }
       }
 
-      if (height > this.options.height) {
-        position.top = (height - this.options.height) * -this.options.offset;
+      return position;
+    },
+
+    setMaxDimensions: function() {
+      if (this.square) {
+        this.$el.css({
+          maxHeight: '100%',
+          maxWidth: '100%'
+        });
+      } else {
+        this.$el.css({
+          maxHeight: (this.landscape ? this.options.height : 'none'),
+          maxWidth: (this.portrait ? this.options.width : 'none')
+        });
       }
-    }
+    },
 
-    return position;
-  };
+    getDimensions: function() {
+      var position = this.getPosition();
 
-  SimpleCrop.prototype.setMaxDimensions = function() {
-    if (this.square) {
-      this.$el.css({
-        maxHeight: '100%',
-        maxWidth: '100%'
-      });
-    } else {
-      this.$el.css({
-        maxHeight: (this.landscape ? this.options.height : 'none'),
-        maxWidth: (this.portrait ? this.options.width : 'none')
-      });
-    }
-  };
+      return {
+        height: this.getScaleHeight(),
+        width: this.getScaleWidth(),
+        left: position.left * this.options.scale,
+        top: position.top * this.options.scale
+      };
+    },
 
-  SimpleCrop.prototype.getDimensions = function() {
-    var position = this.getPosition();
+    getScaleHeight: function() {
+      return this.options.height * this.options.scale;
+    },
 
-    return {
-      height: this.getScaleHeight(),
-      width: this.getScaleWidth(),
-      left: position.left * this.options.scale,
-      top: position.top * this.options.scale
-    };
-  };
+    getScaleWidth: function() {
+      return this.options.width * this.options.scale;
+    },
 
-  SimpleCrop.prototype.getScaleHeight = function() {
-    return this.options.height * this.options.scale;
-  };
-
-  SimpleCrop.prototype.getScaleWidth = function() {
-    return this.options.width * this.options.scale;
-  };
-
-  SimpleCrop.prototype.getPosition = function() {
-    return this.$el.position();
-  };
+    getPosition: function() {
+      return this.$el.position();
+    },
+  });
 
   $.fn[pluginName] = function (options) {
     this.each(function() {
